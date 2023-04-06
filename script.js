d81f540c7ba5a811a135ed96aad2a243e3cfb8f0
Digvijay Kadam <digvi@Digvijays-MacBook-Air.local>
2023-04-06 13:13:40 +0530

    Dashboard Repo Created

---
+++ script.js
@@ -0,0 +1,11 @@
+
+
+const mediaQuery = window.matchMedia('(max-width: 900px)');
+
+function handleWidthChange(e) {
+  if (e.matches) {
+    window.alert('Please Shift to Desktop For Better Experience 😃');
+  }
+}
+
+mediaQuery.addEventListener('change', handleWidthChange);
